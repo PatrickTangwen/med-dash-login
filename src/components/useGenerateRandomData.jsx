49ce418a06ec3a5bb8a954615bbbe530849362f5
PatrickTangwen <102566928+PatrickTangwen@users.noreply.github.com>
2024-03-11 14:45:43 -0700

    card title fixed

--- src/components/useGenerateRandomData.jsx
+++ src/components/useGenerateRandomData.jsx
@@ -1,18 +1,19 @@
 // generate random data for testing in cards
-const useGenerateRandomData = () => {
+const useGenerateCardData = () => {
   let uId = 0;
   const colors = ['#5ec9db', '#f5b97a', '#f57a7a', '#d5d97a'];
-
+  const names = ["Calories Burned", "Distance Traveled", "Heart Rate", "Steps Taken"]
   return () => {
     uId++;
     const colorIndex = (uId - 1) % colors.length;
+    const nameIndex = (uId - 1) % names.length;
     const rawData = new Array(10).fill(null).map(() => Math.round(Math.random() * 200));
     const mean = rawData.reduce((acc, val) => acc + val, 0) / rawData.length;
     const max = Math.max(...rawData);
     const min = Math.min(...rawData);
 
     return {
-      title: `Data Set ${uId}`,
+      title: names[nameIndex],
       id: uId,
       color: colors[colorIndex],
       list: [
@@ -24,4 +25,4 @@ const useGenerateRandomData = () => {
   };
 };
 
-export default useGenerateRandomData;
+export default useGenerateCardData;
